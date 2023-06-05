430d898d74034e0d4be709c89cde84acd37a632d
lamichhaneanita <lamichhaneanita103@gmail.com>
2023-06-05 17:13:24 +0545

    New Changes

--- Day5/alert.js
+++ Day5/alert.js
@@ -1,28 +1,30 @@
+let total=0;
+
 function add(){
    let firstnumber=document.getElementById("num1").value;
    let secondnumber=document.getElementById("num2").value;
-   let total=parseInt(firstnumber) + parseInt(secondnumber);
+   total=parseInt(firstnumber) + parseInt(secondnumber);
    document.getElementById("output").value = String(total);
 
 }
 function sub(){
     let firstnumber=document.getElementById("num1").value;
     let secondnumber=document.getElementById("num2").value;
-    let total=parseInt(firstnumber) - parseInt(secondnumber);
+    total=parseInt(firstnumber) - parseInt(secondnumber);
     document.getElementById("output").value = String(total);
  
  }
  function mul(){
     let firstnumber=document.getElementById("num1").value;
     let secondnumber=document.getElementById("num2").value;
-    let total=parseInt(firstnumber) * parseInt(secondnumber);
+    total=parseInt(firstnumber) * parseInt(secondnumber);
     document.getElementById("output").value = String(total);
  
  }
  function div(){
     let firstnumber=document.getElementById("num1").value;
     let secondnumber=document.getElementById("num2").value;
-    let total=parseInt(firstnumber) / parseInt(secondnumber);
+    total=parseInt(firstnumber) / parseInt(secondnumber);
     document.getElementById("output").value = String(total);
  
  }
