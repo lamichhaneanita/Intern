bdf0d2ac5da4b23191c4e15d67ba0cc27f04a3d0
lamichhaneanita <lamichhaneanita103@gmail.com>
2023-06-08 07:55:56 +0545

    Final LoginPage

--- Day7/script.js
+++ Day7/script.js
@@ -1,41 +1,81 @@
-function Register(){
-    let fname = document.getElementById("name1").value;
-    let Sname = document.getElementById("name2").value;
-    let Email = document.getElementById("email").value;
-    let NewPassword = document.getElementById("password1").value;
-    let ConfirmPassword= document.getElementById("password2").value;
+// function Register(){
+//     let fname = document.getElementById("name1").value;
+//     let Sname = document.getElementById("name2").value;
+//     let Email = document.getElementById("email").value;
+//     let NewPassword = document.getElementById("password1").value;
+//     let ConfirmPassword= document.getElementById("password2").value;
    
 
-     let LoginData = {
-        firstName : fname,
-        lastName : Sname,
-        email :  Email,
-        password : NewPassword,
-        password2 : ConfirmPassword
+//      let LoginData = {
 
-     };
+//         firstName : fname,
+//         lastName : Sname,
+//         email :  Email,
+//         password : NewPassword,
+//         password2 : ConfirmPassword
 
-      fetch("https://rest-api-bjno.onrender.com/register", {
-        method: "POST",
+//      };
+
+//       fetch("https://rest-api-bjno.onrender.com/register", {
+//         method: "POST",
+//         headers: {
+//             "Content-type" :"application/json"
+//         },
+//         body: JSON.stringify(LoginData),
+//       })
+//       .then(res => res.json())
+//       .then(Data =>{
+//         console.log(Data)
+//         if(Data.success){
+//            document.getElementById("message").textContent = "LoginSuccess";
+//         }
+//         else{
+//             document.getElementById("message").textContent = "LoginUnSuccess";
+//         }
+
+//       })
+//       .catch(error => {
+//         console.error("Error:", error);
       
-        headers: {
-            "Content-type" :"application/json"
-        },
-        body: JSON.stringify(LoginData),
-      })
-      .then(res => res.json())
-      .then(Data =>{
-        console.log(Data)
-        if(Data.success){
-           document.getElementById("message").textContent = "LoginSuccess";
-        }
-        else{
-            document.getElementById("message").textContent = "LoginUnSuccess";
-        }
+//     })
+// }  
 
-      })
-      .catch(error => {
-        console.error("Error:", error);
-      
-    })
+ async function Register(){
+  let fname = document.getElementById("name1").value;
+  let Sname = document.getElementById("name2").value;
+  let Email = document.getElementById("email").value;
+  let NewPassword = document.getElementById("password1").value;
+  let ConfirmPassword= document.getElementById("password2").value;
+ 
+
+   let LoginData = {
+
+      firstName : fname,
+      lastName : Sname,
+      email :  Email,
+      password : NewPassword,
+      password2 : ConfirmPassword
+
+   };
+   try{
+  const response = await fetch("https://rest-api-bjno.onrender.com/register", {
+  method: "POST",
+  headers: {
+      "Content-type" :"application/json"
+  },
+  body: JSON.stringify(LoginData),
+})
+  if(response.ok){
+     const data = await response.json();
+     console.log(data);
+  }
+  else{
+     console.log('Error:',response.status)
+  }
+
+}
+catch(error) {
+  console.log("Error:", error);
+
+}
 }   
